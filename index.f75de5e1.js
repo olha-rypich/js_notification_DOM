7f85317c58435350103e569426a625260bac67cc
Olha Rypich <olha.rypich@gmail.com>
2024-07-15 19:09:17 +0300

    make build

---
+++ index.f75de5e1.js
@@ -0,0 +1,20 @@
+"use strict";
+const pushNotification = (posTop, posRight, title, description, type)=>{
+    const message = document.createElement("div");
+    message.style.top = `${posTop}px`;
+    message.style.right = `${posRight}px`;
+    message.classList.add("notification", `${type}`);
+    message.insertAdjacentHTML("afterbegin", `
+    <h2 class="title">${title}</h2>
+    <p>${description}</p>
+  `);
+    document.body.append(message);
+    setTimeout(()=>{
+        message.style.display = "none";
+    }, 2000);
+};
+pushNotification(10, 10, "Title of Success message", "Message example.\n Notification should contain title and description.", "success");
+pushNotification(150, 10, "Title of Error message", "Message example.\n Notification should contain title and description.", "error");
+pushNotification(290, 10, "Title of Warning message", "Message example.\n Notification should contain title and description.", "warning");
+
+//# sourceMappingURL=index.f75de5e1.js.map
